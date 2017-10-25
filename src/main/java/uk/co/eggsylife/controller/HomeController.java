4ab9fc90b878e10832615b5e30da6a33c3929858
Ofir Makmal <ofir.makmal@gmail.com>
2017-10-25 18:14:33 +0300

    Testing CI 2

--- src/main/java/uk/co/eggsylife/controller/HomeController.java
+++ src/main/java/uk/co/eggsylife/controller/HomeController.java
@@ -11,6 +11,6 @@ public class HomeController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showHomePage() {
-        return "<h2>version 1.0.5</h2><p>Hello everyone</p>";
+        return "<h2>version 1.0.6</h2><p>Hello everyone</p>";
     }
 }
